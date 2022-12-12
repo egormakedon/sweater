a42864154f96fea34bb6964eae818877f890d8f5
Yahor Makedon <emakedonw@gmail.com>
2022-12-12 20:57:44 +0100

    Add encoding filter.

--- src/main/java/by/makedon/sweater/config/WebConfig.java
+++ src/main/java/by/makedon/sweater/config/WebConfig.java
@@ -1,7 +1,10 @@
 package by.makedon.sweater.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +17,14 @@ import java.time.Duration;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	@Bean
+	public FilterRegistrationBean<CharacterEncodingFilter> encodingFilter() {
+		FilterRegistrationBean<CharacterEncodingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new CharacterEncodingFilter("UTF-8", true, true));
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**")
